57a511f067246ed8a95f9c764b7d75ba7e7a6ca0
Zain Raza <zainoakgrove@gmail.com>
2021-04-08 10:33:48 -0500

    Initial Commit

---
+++ Currency Converter/Currency_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ConverterApp.swift
+//  Currency Converter
+//
+//  Created by Student on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
