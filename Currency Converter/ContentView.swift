afc67d32200005c3c0738a3d05d89da5d0a6de3d
Yawar Kamran <yawarkamran316@gmail.com>
2021-04-20 10:17:21 -0500

    Added Title/Button

--- Currency Converter/ContentView.swift
+++ Currency Converter/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            NavigationLink(destination: Text("SecondView")) {
+                Text("Convert")
+            }
+            .navigationBarTitle("Currency Converter")
+        }
     }
 }
 
